bf7ca60ebb43202eb58e83519d47605537da9701
avantgov <alexcouts18@gmail.com>
2020-10-15 23:22:45 -0500

    completed guided porject + notes

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,9 +1,12 @@
 """
-Given only a reference to a specific node in a linked list, delete that node from a singly-linked list.
+Given only a reference to a specific node in a linked list, delete that node from a 
+singly-linked list.
 
 Example:
 
-The code below should first construct a linked list (x -> y -> z) and then delete `y` from the linked list by calling the function `delete_node`. It is your job to write the `delete_node` function.
+The code below should first construct a linked list (x -> y -> z) and then delete `y` 
+from the linked list by calling the function `delete_node`. It is your job to write 
+the `delete_node` function.
 
 ```python
 class LinkedListNode():
@@ -23,13 +26,36 @@ delete_node(y)
 
 *Note: We can do this in O(1) time and space! But be aware that our solution will have some side effects...*
 """
+
+"""
+UNDERSTAND: 
+- we do not have access to the head or tail in this situation, just the given node 
+
+- since we only have access to the given item, we will want to look at the next item,
+replace the given item with the next item, then remove the next item and remove 
+the pointer 
+    - we are able to do this since accessing the next node also gives us
+    access to the pointer of the next node as well 
+
+PLAN 
+- set the value of the node_delete to the value of node_delete.next.value 
+- set the pointer node_delete.next to node_delete.next.next
+
+these have to be done in this order so that we do not mess up operations 
+
+"""
 class LinkedListNode():
     def __init__(self, value):
         self.value = value
         self.next  = None
 
 def delete_node(node_to_delete):
-    # Your code here
+    # set the value of the next item to the value of the current item 
+    node_to_delete.value = node_to_delete.next.value
+    # set the pointer of the current item to point to the pointer of the 
+    # next item
+    node_to_delete.next = node_to_delete.next.next
+
 
 
 x = LinkedListNode('X')
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -13,27 +13,88 @@ once.
 running your function by hand. Does it actually work? Also, don't forget to
 consider edge cases (like a list with only 1 or 0 elements).*
 """
+
+
+"""
+UNDERSTAND: 
+given the refernce to the head of a linked list, we want to reverse the list 
+and return the new head 
+    - this involves swapping the position of the tail and head 
+    - and changing all of the pointers in the list 
+    - we cannot just reverse the order, since it isn't really an "order" inherently 
+
+PLAN 
+# Simple and tail recursive Python program to 
+# reverse a linked list 
+
+# Node class 
+class Node: 
+	# Constructor to initialize the node object 
+	def __init__(self, data): 
+		self.data = data 
+		self.next = None
+
+class LinkedList: 
+	# Function to initialize head 
+
+	def reverseUtil(self, curr, prev): 
+		
+		# If last node mark it head 			
+			# Update next to prev node 
+
+		
+		# Save curr.next node for recursive call 
+
+
+		# And update next 
+
+
+	# This function mainly calls reverseUtil() 
+	# with previous as None 
+
+
+
+	# Function to insert a new node at the beginning 
+
+
+	# Utility function to print the linked LinkedList 
+
+
+"""
+
 class LinkedListNode():
+    # inits node object 
     def __init__(self, value):
         self.value = value
         self.next  = None
 
+    def reverseUtil(self, current, previous):
+        # if the current node in focus in the tail (.next is None), make that node the head
+        # then update the previous node  
+        if current.next is None:
+            self.head = current
+            current.next = previous 
+            return 
+        
+        # create holder for the current node in focus's next item for use later 
+        cur_nxt = current.next
+
+        # change the current next to the previous item 
+        current.next = previous
+
+        
+    # This function mainly calls reverseUtil() with previous as None 
+    def reverse(self): 
+        if self.head is None: 
+            return 
+        self.reverseUtil(self.head, None) 
+  
+  
+    # Function to insert a new node at the beginning 
+    def push(self, new_data): 
+        new_node = Node(new_data) 
+        new_node.next = self.head 
+        self.head = new_node 
+  
+
 def reverse(head_of_list):
-    current_node = head_of_list
-    previous_node = None
-    next_node = None
-
-    # Until we have 'fallen off' the end of the list
-    while current_node:
-        # Copy a pointer to the next element
-        # before we overwrite current_node.next
-        next_node = current_node.next
-
-        # Reverse the 'next' pointer
-        current_node.next = previous_node
-
-        # Step forward in the list
-        previous_node = current_node
-        current_node = next_node
-
-    return previous_node
